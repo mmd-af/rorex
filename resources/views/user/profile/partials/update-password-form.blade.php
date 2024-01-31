9f8354d3e9bd0e8f54a8dec7690768f86d742ebe
afshar-dev <thepermonica@gmail.com>
2024-01-31 10:00:25 +0200

    Profile in finished

--- resources/views/user/profile/partials/update-password-form.blade.php
+++ resources/views/user/profile/partials/update-password-form.blade.php
@@ -9,38 +9,29 @@
         </p>
     </header>
 
-    <form method="post" action="{{ route('password.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('password.update') }}" class="mt-6 col-md-6">
         @csrf
         @method('put')
 
         <div>
-            <x-input-label for="update_password_current_password" :value="__('Current Password')" />
-            <x-text-input id="update_password_current_password" name="current_password" type="password" class="mt-1 block w-full" autocomplete="current-password" />
-            <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2" />
-        </div>
-
+            <label for="update_password_current_password">{{__('Current Password')}}</div>
+        <input id="update_password_current_password" name="current_password" type="password"
+               class="form-control" autocomplete="current-password"/>
+        <x-input-error :messages="$errors->updatePassword->get('current_password')" class="mt-2 text-danger" />
         <div>
-            <x-input-label for="update_password_password" :value="__('New Password')" />
-            <x-text-input id="update_password_password" name="password" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2" />
-        </div>
-
+            <label for="update_password_password">{{__('New Password')}}</div>
+        <input id="update_password_password" name="password" type="password" class="form-control"
+               autocomplete="new-password"/>
+        <x-input-error :messages="$errors->updatePassword->get('password')" class="mt-2 text-danger" />
         <div>
-            <x-input-label for="update_password_password_confirmation" :value="__('Confirm Password')" />
-            <x-text-input id="update_password_password_confirmation" name="password_confirmation" type="password" class="mt-1 block w-full" autocomplete="new-password" />
-            <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2" />
-        </div>
-
+            <label for="update_password_password_confirmation">{{__('Confirm Password')}}</div>
+        <input id="update_password_password_confirmation" name="password_confirmation" type="password"
+               class="form-control" autocomplete="new-password"/>
+        <x-input-error :messages="$errors->updatePassword->get('password_confirmation')" class="mt-2 text-danger" />
         <div class="flex items-center gap-4">
-            <x-primary-button>{{ __('Save') }}</x-primary-button>
-
+            <button type="submit" class="btn btn-success mt-3">{{ __('Save') }}</button>
             @if (session('status') === 'password-updated')
-                <p
-                    x-data="{ show: true }"
-                    x-show="show"
-                    x-transition
-                    x-init="setTimeout(() => show = false, 2000)"
-                    class="text-sm text-gray-600"
+                <p class="text-sm text-success"
                 >{{ __('Saved.') }}</p>
             @endif
         </div>
--- resources/views/user/profile/partials/update-profile-information-form.blade.php
+++ resources/views/user/profile/partials/update-profile-information-form.blade.php
@@ -3,20 +3,16 @@
         <h2 class="text-lg font-medium text-gray-900">
             {{ __('Profile Information') }}
         </h2>
-
         <p class="mt-1 text-sm text-gray-600">
             {{ __("Update your account's profile information and email address.") }}
         </p>
     </header>
-
     <form id="send-verification" method="post" action="{{ route('verification.send') }}">
         @csrf
     </form>
-
-    <form method="post" class="col-md-6" action="{{ route('profile.update') }}" class="mt-6 space-y-6">
+    <form method="post" action="{{ route('profile.update') }}" class="mt-6 col-md-6">
         @csrf
         @method('patch')
-
         <div>
             <label for="name">{{__('Name')}}</div>
         <input id="name" name="name" type="text" class="form-control" value="{{old('name', $user->name)}}" required
@@ -41,7 +37,6 @@
                             {{ __('Click here to re-send the verification email.') }}
                         </button>
                     </p>
-
                     @if (session('status') === 'verification-link-sent')
                         <p class="mt-2 font-medium text-sm text-green-600">
                             {{ __('A new verification link has been sent to your email address.') }}
@@ -50,13 +45,10 @@
                 </div>
             @endif
         </div>
-
         <div class="flex items-center gap-4">
             <button type="submit" class="btn btn-success mt-3">{{ __('Save') }}</button>
-
             @if (session('status') === 'profile-updated')
-                <p class="text-sm text-success"
-                >{{ __('Saved.') }}</p>
+                <p class="text-sm text-success">{{ __('Saved.') }}</p>
             @endif
         </div>
     </form>
