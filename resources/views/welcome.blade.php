b880c5cc03f174ce0caf6536a19777bd034aae1d
afshar-dev <thepermonica@gmail.com>
2024-02-12 15:49:19 +0200

    Refactor

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,11 +15,22 @@
 <div class="container">
     <div class="row justify-content-center">
         <img class="img-fluid w-25" src="{{asset('build/img/logo.png')}}" alt="ROREX - PIPE">
-        <div class="d-flex justify-content-center">
-            <a class="btn btn-outline-info text-primary mx-2" href="{{route('login')}}">Login</a>
-            <a class="btn btn-outline-info text-primary mx-2" href="{{route('register')}}">Register</a>
-        </div>
+        @if (Route::has('login'))
+            <div class="d-flex justify-content-center">
+                @auth
+                    <a href="{{ route('user.dashboard.index') }}"
+                       class="btn btn-outline-info text-primary mx-2">Dashboard</a>
+                @else
+                    <a class="btn btn-outline-info text-primary mx-2" href="{{route('login')}}">Login</a>
+                    @if (Route::has('register'))
+                        <a class="btn btn-outline-info text-primary mx-2" href="{{route('register')}}">Register</a>
+                    @endif
+                @endauth
+            </div>
+
     </div>
+    @endif
+</div>
 </div>
 
 </body>
