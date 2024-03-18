21f199ee3de82e7e9e8e02eae0101d790607c270
afshar-dev <thepermonica@gmail.com>
2024-03-18 11:46:52 +0200

    Refactor Navbar

--- resources/views/admin/layouts/partial/navbar.blade.php
+++ resources/views/admin/layouts/partial/navbar.blade.php
@@ -1,17 +1,24 @@
-<nav class="sb-topnav navbar navbar-expand navbar-dark" style="background-color: #212529">
-    <!-- Navbar Brand-->
+<nav class="sb-topnav navbar navbar-expand navbar-dark px-3" style="background-color: #212529">
+    <button class="btn btn-link btn-sm bg-white text-dark" id="sidebarToggle" href="#!">
+        <i class="fas fa-bars"></i>
+    </button>
     <a class="navbar-brand ps-3" href="{{url('/')}}">rorex.ro</a>
-    <!-- Sidebar Toggle-->
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i
-            class="fas fa-bars"></i></button>
     <!-- Navbar Search-->
     <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
         <div class="input-group">
             <input class="form-control" type="text" placeholder="Search for..." aria-label="Search for..."
                    aria-describedby="btnNavbarSearch"/>
-            <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
+            <button class="btn btn-primary" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i>
+            </button>
         </div>
     </form>
+    <button type="button" class="btn position-relative mx-3 mt-3">
+        <i class="fa-regular fa-envelope text-white"></i>
+        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+            +3
+                        <span class="visually-hidden">unread messages</span>
+                     </span>
+    </button>
     <!-- Navbar-->
     <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
         <li class="nav-item dropdown">
@@ -26,7 +33,6 @@
                 <li>
                     <form method="POST" action="{{ route('logout') }}">
                         @csrf
-
                         <button class="btn" onclick="event.preventDefault();
                                                 this.closest('form').submit();">
                             {{ __('Log Out') }}
