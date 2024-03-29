622ff25e515e904cc0391bf14403f49f6dc74402
afshar.dev <thepermonica@gmail.com>
2024-03-29 14:51:03 +0200

    Add CSV format to upload files

--- app/Http/Requests/Admin/User/UserUpdateRequest.php
+++ app/Http/Requests/Admin/User/UserUpdateRequest.php
@@ -24,7 +24,7 @@ class UserUpdateRequest extends FormRequest
     public function rules()
     {
         return [
-            'file' => 'required|mimes:xlsx,xls'
+            'file' => 'required|mimes:xlsx,xls,csv'
         ];
     }
 }
