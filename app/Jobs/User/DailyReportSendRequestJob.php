decc7adde24a8c3376bd902ca687a71f972d2ec8
afshar.dev <thepermonica@gmail.com>
2024-04-16 12:31:25 +0300

    Refactor Job/Queue

--- app/Jobs/User/DailyReportSendRequestJob.php
+++ app/Jobs/User/DailyReportSendRequestJob.php
@@ -28,30 +28,38 @@ class DailyReportSendRequestJob implements ShouldQueue
     /**
      * Execute the job.
      */
+    // Log::info("Result" . $staffRequest->name . " " . $assignment);
+
     public function handle(): void
     {
-        $staffRequest = new StaffRequest();
-        $staffRequest->name = $this->data['name'];
-        $staffRequest->user_id = $this->data['userId'];
-        $staffRequest->email = $this->data['email'];
-        $staffRequest->mobile_phone = $this->data['mobile_phone'];
-        $staffRequest->subject = $this->data['subject'];
-        $staffRequest->description = $this->data['description'];
-        $staffRequest->organization = $this->data['organization'];
-        $staffRequest->cod_staff = $this->data['cod_staff'];
-        $staffRequest->vacation_day = $this->data['vacation_day'];
-        $staffRequest->save();
-        $role = Role::query()
-            ->select(['id', 'name'])
-            ->where('name', $this->data['departamentRole'])
-            ->first();
-        $assignment = new LetterAssignment();
-        $assignment->user_id = $this->data['userId'];
-        $assignment->request_id = $staffRequest->id;
-        $assignment->role_id = $role->id;
-        $assignment->assigned_to = $this->data['assigned_to'];
-        $assignment->status = "waiting";
-        $assignment->save();
-        Log::info("Result" . $staffRequest->name . " " . $assignment);
+        try {
+            $staffRequest = new StaffRequest();
+            $staffRequest->name = $this->data['name'];
+            $staffRequest->user_id = $this->data['userId'];
+            $staffRequest->email = $this->data['email'];
+            $staffRequest->mobile_phone = $this->data['mobile_phone'];
+            $staffRequest->subject = $this->data['subject'];
+            $staffRequest->description = $this->data['description'];
+            $staffRequest->organization = $this->data['organization'];
+            $staffRequest->cod_staff = $this->data['cod_staff'];
+            $staffRequest->vacation_day = $this->data['vacation_day'];
+            $staffRequest->save();
+            $role = Role::query()
+                ->select(['id', 'name'])
+                ->where('name', $this->data['departamentRole'])
+                ->first();
+            $assignment = new LetterAssignment();
+            $assignment->user_id = $this->data['userId'];
+            $assignment->request_id = $staffRequest->id;
+            $assignment->role_id = $role->id;
+            $assignment->assigned_to = $this->data['assigned_to'];
+            $assignment->status = "waiting";
+            $assignment->save();
+            // event(new RequestRegistered($staffRequest, $assignment));
+            // return response()->json(['message' => 'Succecsd'], 500);
+        } catch (\Exception $e) {
+            // Log::error("Error: " . $e->getMessage());
+            // return response()->json(['error' => 'An error occurred. Please try again later.'], 500);
+        }
     }
 }
