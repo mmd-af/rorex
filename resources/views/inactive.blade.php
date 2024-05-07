959a75d7a833f8cb4042b141096184d5632d8300
afshar.dev <thepermonica@gmail.com>
2024-05-07 08:49:01 +0300

    Redesign inactive page

--- resources/views/inactive.blade.php
+++ resources/views/inactive.blade.php
@@ -1,15 +1,43 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Rorex Pipe</title>
+
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+    @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 
 <body>
-    You account was disabled
+    <div class="container">
+        <div class="row justify-content-center">
+            <img class="img-fluid w-25" src="{{ asset('build/img/logo.png') }}" alt="ROREX - PIPE">
+            @if (Route::has('login'))
+                <div class="row text-center">
+                    @auth
+                        <div class="col-12">
+                            <h3 class="text-danger">You account is disable</h3>
+                        </div>
+                        <div class="col-12">
+                            <form method="POST" action="{{ route('logout') }}">
+                                @csrf
+                                <button class="btn btn-outline-info text-primary mx-2"
+                                    onclick="event.preventDefault();
+                                                this.closest('form').submit();">
+                                    {{ __('Log Out') }}
+                                </button>
+                            </form>
+                        </div>
+                    @endauth
+                </div>
+            @endif
+        </div>
+    </div>
 </body>
 
 </html>
