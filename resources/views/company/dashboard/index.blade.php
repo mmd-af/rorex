98c39b2ed19f35344f18331c922102c874600e1a
afshar.dev <thepermonica@gmail.com>
2024-05-29 15:39:48 +0300

    Add CMR to Company panel

--- resources/views/company/dashboard/index.blade.php
+++ resources/views/company/dashboard/index.blade.php
@@ -33,20 +33,10 @@
                                             <a class="list-group-item list-group-item-action" id="list-profile-list"
                                                 data-bs-toggle="list" href="#invoice-{{ $transport->id }}" role="tab"
                                                 aria-controls="list-profile"><b>Invoice</b></a>
+                                            <a class="list-group-item list-group-item-action" id="list-profile-list"
+                                                data-bs-toggle="list" href="#cmr-{{ $transport->id }}" role="tab"
+                                                aria-controls="list-profile"><b>CMR</b></a>
                                         @endif
-
-                                        {{-- <div class="mt-4">
-                                                <form action="{{ route('admin.orders.uploadCmr') }}" method="post" enctype="multipart/form-data">
-                                                    @csrf
-                                                    <input type="hidden" name="order_id" value="{{ $order->id }}">
-                                                    <div class="input-group mb-3">
-                                                        <input type="file" name="cmr" class="form-control form-control-sm" id="cmr"
-                                                            required>
-                                                        <label class="input-group-text" for="cmr">Add CMR</label>
-                                                    </div>
-                                                    <button type="submit" class="btn btn-success btn-sm">upload</button>
-                                                </form>
-                                            </div>                                            --}}
                                     @endif
                                 </div>
                             </div>
@@ -165,13 +155,16 @@
                                                                 </div>
                                                                 <a href="{{ asset($invoice->invoice) }}"
                                                                     target="_blank">invoice-{{ $invoice->id }}</a>
-                                                                    <form class="float-end" action="{{ route('company.dashboard.invoiceDestroy', $invoice->id) }}"
-                                                                        method="POST" onsubmit="return confirm('Are you sure you want to delete this file?');">
-                                                                        @csrf
-                                                                        @method('DELETE')
-                                                                        <button type="submit" class="btn btn-danger btn-sm"> <i class="fa fa-trash"
-                                                                                aria-hidden="true"></i> </button>
-                                                                    </form>
+                                                                <form class="float-end"
+                                                                    action="{{ route('company.dashboard.invoiceDestroy', $invoice->id) }}"
+                                                                    method="POST"
+                                                                    onsubmit="return confirm('Are you sure you want to delete this file?');">
+                                                                    @csrf
+                                                                    @method('DELETE')
+                                                                    <button type="submit" class="btn btn-danger btn-sm"> <i
+                                                                            class="fa fa-trash" aria-hidden="true"></i>
+                                                                    </button>
+                                                                </form>
                                                             </div>
                                                         </li>
                                                     @endforeach
@@ -195,6 +188,23 @@
                                                             class="btn btn-success btn-sm">upload</button>
                                                     </form>
                                                 </div>
+                                            </div>
+                                            <div class="tab-pane fade" id="cmr-{{ $transport->id }}" role="tabpanel"
+                                                aria-labelledby="list-home-list">
+                                                <h3>CMR</h3>
+                                                <ol class="list-group list-group-numbered">
+                                                    @foreach ($orders->where('transportation_id', $transport->id)->first()->cmrOrders as $cmr)
+                                                        <li
+                                                            class="list-group-item d-flex justify-content-between align-items-start">
+                                                            <div class="ms-2 me-auto">
+                                                                <div class="fw-bold">Tracking Number: {{ $cmr->id }}
+                                                                </div>
+                                                                <a href="{{ asset($cmr->cmr) }}"
+                                                                    target="_blank">cmr-{{ $cmr->id }}</a>
+                                                            </div>
+                                                        </li>
+                                                    @endforeach
+                                                </ol>
                                             </div>
                                         @endif
                                     @endif
