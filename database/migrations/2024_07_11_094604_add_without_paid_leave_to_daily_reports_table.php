be8b67563ceef960141d5daf6f329c5104e57fb5
afshar.dev <thepermonica@gmail.com>
2024-07-11 10:04:32 +0300

    Update daily_reports Migration

---
+++ database/migrations/2024_07_11_094604_add_without_paid_leave_to_daily_reports_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('daily_reports', function (Blueprint $table) {
+            $table->string('without_paid_leave')->nullable()->after('concediu_ore');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('daily_reports', function (Blueprint $table) {
+            $table->dropColumn('without_paid_leave');
+        });
+    }
+};
