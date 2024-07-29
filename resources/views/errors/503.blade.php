06d5dce5681502e817930f6bf4a0dc98e3e344f2
afshar.dev <thepermonica@gmail.com>
2024-07-29 10:14:38 +0300

    feat: Add 503 blade file

---
+++ resources/views/errors/503.blade.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="fa">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Site is Under Maintenance</title>
+    <style>
+        body {
+            text-align: center;
+            padding: 0;
+            margin: 0;
+            font-family: "Helvetica Neue", Helvetica, Arial, sans-serif;
+            background-color: #009799;
+            color: #333;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            height: 100vh;
+        }
+        .container {
+            background-color: #fff;
+            padding: 30px;
+            border-radius: 10px;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            max-width: 600px;
+            width: 90%;
+            text-align: center;
+        }
+        h1 {
+            font-size: 36px;
+            margin-bottom: 20px;
+            color: #009799;
+        }
+        p {
+            font-size: 18px;
+            margin-bottom: 20px;
+            line-height: 1.6;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Site is Under Maintenance</h1>
+        <p>Please check back later. The site will be available soon.</p>
+
+        <h1>Site-ul este în mentenanță</h1>
+        <p>Vă rugăm să reveniți mai târziu. Site-ul va fi disponibil în curând.</p>
+
+        <h1>سایت در حال بروزرسانی است</h1>
+        <p>لطفاً بعداً مراجعه کنید. سایت به زودی در دسترس خواهد بود.</p>
+    </div>
+</body>
+</html>
