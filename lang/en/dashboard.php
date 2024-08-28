845225863ebf5a1166521264852dc6a79b9d19e7
afshar.dev <thepermonica@gmail.com>
2024-08-28 10:40:10 +0300

    Update Language

--- lang/en/dashboard.php
+++ lang/en/dashboard.php
@@ -5,5 +5,9 @@ return [
     'user_dashboard' => 'User Dashboard',
     'register_your_email' => 'To receive notifications and access certain features, register your email in the system.',
     'first_name' => 'First Name',
-    'last_name' => 'Last Name'
+    'last_name' => 'Last Name',
+    'need_assistant' => 'Need Assistance?',
+    'dont_hesitate_to_ask' => 'Don\'t hesitate to ask! We are here to help you.',
+    'your_message' => 'Your Message',
+    'send' => 'Send'
 ];
--- lang/ro/dashboard.php
+++ lang/ro/dashboard.php
@@ -5,5 +5,9 @@ return [
     'user_dashboard' => 'Panou Utilizator',
     'register_your_email' => 'Pentru a primi notificări și a accesa anumite funcții, înregistrați-vă email-ul în sistem.',
     'first_name' => 'Prenume',
-    'last_name' => 'Nume de Familie'
+    'last_name' => 'Nume de Familie',
+    'need_assistant' => 'Aveți nevoie de asistență?',
+    'dont_hesitate_to_ask' => 'Nu ezitați să întrebați! Suntem aici pentru a vă ajuta.',
+    'your_message' => 'Mesajul tău',
+    'send' => 'Trimite'
 ];
